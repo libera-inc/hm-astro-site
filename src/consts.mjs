80130a856b5dc8b063f3b3e26bf99b91f58e80c7
sugiyama gaku <gaku@libera-inc.co.jp>
2023-10-19 13:05:37 +0900

    OGP画像の変更

--- src/consts.mjs
+++ src/consts.mjs
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Astro勉強";
 export const SITE_DESCRIPTION = "Astroの勉強用サイトです。";
-export const SITE_URL = "https://hm-astro-site-hk4hxlj6o-libera.vercel.app/";
+export const SITE_URL = "https://hm-astro-site.vercel.app/";
