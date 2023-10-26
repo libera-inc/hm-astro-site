133b338b7fb5416be2adb1afa6b891a6f8e90497
sugiyama gaku <gaku@libera-inc.co.jp>
2023-10-26 11:06:18 +0900

    スキーマ定義の修正

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,39 @@
 import { defineCollection, z } from "astro:content";
 
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+// 共通のバリデーション関数
+const validateDateFormat = (value: string) => dateRegex.test(value);
+const validateDateFormatOptional = (value: string | undefined) => (value ? validateDateFormat(value) : true);
+
+const dateField = z.string().refine(validateDateFormat, {
+    message: "YYYY-MM-DDのフォーマットにしてください。",
+});
+
+const dateFieldOptional = z.string().optional().refine(validateDateFormatOptional, {
+    message: "YYYY-MM-DDのフォーマットにしてください。",
+});
+
+// 共通のスキーマ定義
+const baseSchema = {
+    title: z.string(),
+    description: z.string(),
+    pubDate: dateField,
+    updatedDate: dateFieldOptional,
+};
+
 const blog = defineCollection({
     type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z
-            .string()
-            .or(z.date())
-            .transform((val) => new Date(val)),
-        updatedDate: z
-            .string()
-            .optional()
-            .transform((str) => (str ? new Date(str) : undefined)),
-        thumbnail: z.string().optional(),
-    }),
+    schema: ({ image }) =>
+        z.object({
+            ...baseSchema,
+            thumbnail: image(),
+        }),
 });
 
 const news = defineCollection({
     type: "content",
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z
-            .string()
-            .or(z.date())
-            .transform((val) => new Date(val)),
-        updatedDate: z
-            .string()
-            .optional()
-            .transform((str) => (str ? new Date(str) : undefined)),
-    }),
+    schema: z.object(baseSchema),
 });
 
 export const collections = { blog, news };
